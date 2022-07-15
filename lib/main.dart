36e5723d46d8426fec4367c0c6c83d319d012566
Mahad Saleem <ms.asd871@gmail.com>
2022-07-15 05:39:21 +0500

    color Scheme

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,32 @@ import 'package:flutter/material.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFF0A0D21),
+          primary: Color(0xFF0A0D21),
+          secondary: Color(0xFFEB1555),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0D21),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
       home: InputPage(),
     );
   }
 }
 
 class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
   @override
   _InputPageState createState() => _InputPageState();
 }
@@ -21,13 +38,16 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: Center(
+          child: Text('BMI CALCULATOR'),
+        ),
       ),
       body: Center(
         child: Text('Body Text'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        onPressed: () => InputPage(),
       ),
     );
   }
